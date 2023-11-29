bf8037c0c2484773520df1849c727cdc347c2d68
bikashsrai <bikashrai7924@gmail.com>
2023-11-29 15:58:56 +0545

    typescript build error true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images:{
        domains:['cdn.imagin.studio']
        // domains:['f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com']
+},
+typescript:{
+    ignoreBuildErrors:true,
+
 }
 }
 module.exports = nextConfig
